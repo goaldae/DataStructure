02e4397606bce225e7a6ad0be46ecfcb32937500
goaldae <goaldae@naver.com>
2019-05-15 00:02:15 +0900

    Complete postfix1

--- week10/postfix1.c
+++ week10/postfix1.c
@@ -1,128 +1,141 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#include <string.h>
 #define MAX 100
 
-void push(char*, char);
-int priority(char);
+typedef struct Oper{
+	char data;
+	int pri;
+}oper;
 
 int top = 0;
-char stack[MAX];
 
-void printOp() {
-	if (stack[top] == '$') {
-		printf("%c", '+');
-		top--;
-		return;
-	}else if (stack[top] == '%') {
-		printf("%c", '-');
-		top--;
-		return;
+int priority(char* input, int j)
+{
+	if (input[j] == '+' || input[j] == '-') {
+		if (j == 0 || input[j - 1] == '!' || input[j - 1] == '*' || input[j - 1] == '/' || input[j - 1] == '>'
+			|| input[j - 1] == '<' || input[j - 1] == '&' || input[j - 1] == '|' || input[j - 1] == '+' || input[j - 1] == '-') {
+			return 6;
+		}
+		else
+		{
+			return 4;
+		}
 	}
-	printf("%c", stack[top--]);
+	else
+	{
+		switch (input[j]) {
+		case '!':
+			return 6;
+		case '*':case '/':
+			return 5;
+		case '<':case '>':
+			return 3;
+		case '&':
+			return 2;
+		case '|':
+			return 1;
+		default:
+			return 0;
+		}
+	}
+	
+}
+void push(oper* stack, char* input, int j)
+{
+	top++;
+	stack[top].data = input[j];
+	stack[top].pri = priority(input, j);
+
 }
 
-
-int oper = 1;
+void printStack(oper* stack) {
+	while (stack[top].data!=' ')
+	{
+		if (stack[top].data == '(' || stack[top].data == ')') {
+			top--;
+		}
+		else {
+			printf("%c", stack[top].data);
+			top--;
+		}
+	}
+}
 
 int main(void)
 {
+	char input[100];
 	int n;
-	scanf("%d", &n);
+
+	scanf("%d", &n); //반복횟수
+
 	for (int i = 0; i < n; i++) {
-		char in;
 		
-		stack[0] = 0;
-		in = getchar();
-		while (in != EOF)
-		{
-			if (in == 10) {
-				while (stack[top])
-					printOp();
-				puts("");
+		scanf("%s", input);
+		
+		oper stack[MAX];
+		stack[0].data = ' ';
+		stack[0].pri = 0;
+		
+		for (int j = 0; j < strlen(input); j++) {
+			if (j == strlen(input)-1) {
+				if (input[j] == '(' || input[j] == ')') {
+					printStack(stack);
+				}
+				else
+				{
+					printf("%c", input[j]);
+					printStack(stack);
+				}
+				printf("\n");
+							
 			}
-			else if (in == '(')
-				push(stack, in);
-			else if (in == ')') {
-				while (stack[top] != '(')
-					printOp();
+			else if (input[j] == '(')
+				push(stack, input, j);
+			else if (input[j] == ')') {
+				while (stack[top].data != '(')
+					printf("%c", stack[top--].data);
 				top--;
 			}
-			else if (in == '+' || in == '-' || in == '*' || in == '/' || in == '>' || in == '<' || in == '!') {
-				if (oper == 1) {
-					if (in == '+' ) {
-						while (priority(stack[top]) >= 6)
-							printOp();
-						push(stack, '$');
-					}
-					else if (in == '-') {
-						while (priority(stack[top]) >= 6)
-							printOp();
-						push(stack, '%');
-					}
-					oper = 0;
-				}
-				while (priority(stack[top]) >= priority(in))
-					printOp();
-				push(stack, in);
-				oper = 1;
+			else if (input[j] == '+' || input[j] == '-' || input[j] == '*' || input[j] == '/' || input[j] == '>' || input[j] == '<' || input[j] == '!') {
+				while (stack[top].pri >= priority(input, j))
+					printf("%c", stack[top--].data);
+				push(stack, input, j);
+				
 			}
-			else if (in == '&' || in == '|') {
-				if (in != stack[top]) { //첫번째거일때
-					while (priority(stack[top]) >= priority(in)) {
-						printOp();
+			else if (input[j] == '&' || input[j] == '|') {
+				if (input[j] != stack[top].data) { //첫번째거일때
+					while (stack[top].pri >= priority(input, j)) {
+						printf("%c", stack[top--].data);
 					}
-					push(stack, in);
+					push(stack, input, j);
 				}
 				else {	//두번째꺼일때
-					if (stack[top - 1] == stack[top]) {
-						while (priority(stack[top]) >= priority(in)) {
-							printOp();
+					if (stack[top - 1].data == stack[top].data) {
+						while (stack[top].pri >= priority(input, j)) {
+							printf("%c", stack[top--].data);
 						}
-						push(stack, in);
+						push(stack, input, j);
 					}
 					else {
-						push(stack, in);
+						push(stack, input, j);
 					}
 
 				}
-				oper = 1;
+			
 			}
 			else {
-				oper = 0;
-				printf("%c", in);
+				
+				printf("%c", input[j]);
+				if (stack[top].pri == 6) {
+					printf("%c", stack[top--].data);
+				}
 			}
-			in = getchar();
-		}
+		}	
 	}
 
 	return 0;
 }
 
-void push(char *s, char data)
-{
-	s[++top] = data;
-}
 
-int priority(char c)
-{
-	switch (c) {
-	case '$':
-		return 6;
-	case '%':
-		return 6;
-	case '!':
-		return 6;
-	case '*':case '/':
-		return 5;
-	case '-':case '+':
-		return 4;
-	case '<':case '>':
-		return 3;
-	case '&':
-		return 2;
-	case '|':
-		return 1;
-	default:
-		return 0;
-	}
-}
+
